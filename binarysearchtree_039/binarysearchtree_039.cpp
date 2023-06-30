31868c67b2e5e22c3616f1908152e407ae457d06
windyaan <windyanurmaretha@gmail.com>
2023-06-30 13:28:36 +0700

    method inorder dan menetapkan kondisi

--- binarysearchtree_039/binarysearchtree_039.cpp
+++ binarysearchtree_039/binarysearchtree_039.cpp
@@ -70,4 +70,19 @@ public:
 				currentNode = currentNode->rightchild;
 		}
 	}
+
+	void inorder(Node* ptr)
+	{
+		if (ROOT == NULL)
+		{
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL)
+		{
+			inorder(ptr->leftchild);
+			cout << ptr->info << " ";
+			inorder(ptr->rightchild);
+		}
+	}
 };
