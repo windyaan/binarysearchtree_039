d0ba1cca381db3fe043b2b931bf502fdc6f9dee6
windyaan <windyanurmaretha@gmail.com>
2023-06-30 13:13:50 +0700

    Menetapkan kondisi jika parent == NULL

--- binarysearchtree_039/binarysearchtree_039.cpp
+++ binarysearchtree_039/binarysearchtree_039.cpp
@@ -38,5 +38,11 @@ public:
 		Node* parent = NULL;
 		Node* currentNode = NULL;
 		search(element, parent, currentNode); // Locate the node which will be the parent of the node to be inserted
+
+		if (parent == NULL) // If the parent is NULL (Tree is empty)
+		{
+			ROOT = newNode; // Mark the new node as ROOT
+			return; //Exit
+		}
 	}
 };
