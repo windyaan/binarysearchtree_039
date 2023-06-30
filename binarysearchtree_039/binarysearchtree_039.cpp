697637996e844bd7352250089b1f6dadbed4c599
windyaan <windyanurmaretha@gmail.com>
2023-06-30 13:51:44 +0700

    Edit 1

--- binarysearchtree_039/binarysearchtree_039.cpp
+++ binarysearchtree_039/binarysearchtree_039.cpp
@@ -47,7 +47,7 @@ public:
 
 		if (element < parent->info) // If the value in the data field of the new node is less than that of the parent
 		{
-			parent->rightchild = newNode; // Make the left child of the parent point to the new node
+			parent->leftchild = newNode; // Make the left child of the parent point to the new node
 		}
 		else if (element > parent->info) // If the value in the data field of the new node greater than that of the parent
 		{
