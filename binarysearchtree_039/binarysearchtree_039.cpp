bd086740863f6f7e4713939e3d89c4f212d01fb9
windyaan <windyanurmaretha@gmail.com>
2023-06-30 14:00:07 +0700

    Edit 3

--- binarysearchtree_039/binarysearchtree_039.cpp
+++ binarysearchtree_039/binarysearchtree_039.cpp
@@ -56,7 +56,7 @@ public:
 	}
 
 
-	void search(string element, Node*& currentNode)
+	void search(string element, Node* parent, Node*& currentNode)
 	{
 		// This function searches the currentNode of the specified Node as well as the current Node of its parent
 		currentNode = ROOT;
