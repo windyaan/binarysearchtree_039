c9d749b30ad376946a2f3ace52f233a5a68ec5d4
windyaan <windyanurmaretha@gmail.com>
2023-06-30 13:11:30 +0700

    Menambahkan Node parent

--- binarysearchtree_039/binarysearchtree_039.cpp
+++ binarysearchtree_039/binarysearchtree_039.cpp
@@ -34,5 +34,9 @@ public:
 		newNode->info = element; // Assign value to the data field of the new node
 		newNode->leftchild = NULL; // Make the left child of the new node point to NULL
 		newNode->rightchild = NULL; //Make the right child of the new node point to NULL
+
+		Node* parent = NULL;
+		Node* currentNode = NULL;
+		search(element, parent, currentNode); // Locate the node which will be the parent of the node to be inserted
 	}
 };
